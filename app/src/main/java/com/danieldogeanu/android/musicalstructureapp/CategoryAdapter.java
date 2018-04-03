342598a3c5ba9cd9495658e99c540d259e4c651b
Daniel Dogeanu <contact@danieldogeanu.com>
2018-04-03 20:00:57 +0300

    Added CategoryAdapter Class

---
+++ app/src/main/java/com/danieldogeanu/android/musicalstructureapp/CategoryAdapter.java
@@ -0,0 +1,40 @@
+package com.danieldogeanu.android.musicalstructureapp;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    private final int PAGE_COUNT = 3;
+    private String[] tabTitles = new String[3];
+
+    private Context mContext;
+
+    public CategoryAdapter(FragmentManager fragmentManager, Context context) {
+        super(fragmentManager);
+        mContext = context;
+
+        tabTitles[0] = mContext.getString(R.string.cat_songs);
+        tabTitles[1] = mContext.getString(R.string.cat_artists);
+        tabTitles[2] = mContext.getString(R.string.cat_albums);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // TODO: Add Fragments for each Category first.
+        return null;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+
+}
