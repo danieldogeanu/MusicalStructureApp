85f7e5a768c7140c85e34e27694c5163f8016c69
Daniel Dogeanu <contact@danieldogeanu.com>
2018-04-19 03:21:52 +0300

    Added Album Art Images

--- app/src/main/java/com/danieldogeanu/android/musicalstructureapp/Data.java
+++ app/src/main/java/com/danieldogeanu/android/musicalstructureapp/Data.java
@@ -28,36 +28,36 @@ public class Data {
     }
 
     private void loadData() {
-        mSongs.add(new Song("Done with Disco", "Art of Escapism", "Wild Card", "3:10"));
-        mSongs.add(new Song("Eight Cat", "Art of Escapism", "Wild Card", "3:15"));
-        mSongs.add(new Song("Little by Little", "Art of Escapism", "Wild Card", "1:50"));
-        mSongs.add(new Song("Out on Our Own", "Art of Escapism", "Wild Card", "3:16"));
-        mSongs.add(new Song("Rahjan", "Art of Escapism", "Wild Card", "1:55"));
-        mSongs.add(new Song("Start Over", "Art of Escapism", "Wild Card", "3:12"));
-        mSongs.add(new Song("Awestruck", "Cellophane Sam", "Sea Change", "3:58"));
-        mSongs.add(new Song("Deluge", "Cellophane Sam", "Sea Change", "3:51"));
-        mSongs.add(new Song("Early Breath", "Cellophane Sam", "Sea Change", "3:03"));
-        mSongs.add(new Song("Mountains", "Cellophane Sam", "Sea Change", "2:19"));
-        mSongs.add(new Song("The Turnaround Road", "Cellophane Sam", "Sea Change", "3:05"));
-        mSongs.add(new Song("While Looking Up", "Cellophane Sam", "Sea Change", "3:21"));
-        mSongs.add(new Song("Lifeless Worlds", "DASK", "Abiogenesis", "3:20"));
-        mSongs.add(new Song("Mass Forming", "DASK", "Abiogenesis", "7:12"));
-        mSongs.add(new Song("Junk Vultures", "Exquisite Frosting Penmanship", "Transistor Rodeo", "2:08"));
-        mSongs.add(new Song("Lightbringer", "Magna Ingress", "Aeon 3: Memoriae", "3:27"));
-        mSongs.add(new Song("Nuts and Bolts", "Monkey Warhol", "Hannah Banana", "3:27"));
-        mSongs.add(new Song("Stolen Moments", "Monkey Warhol", "Hannah Banana", "2:59"));
-        mSongs.add(new Song("Times of Your Life", "Monkey Warhol", "Hannah Banana", "3:32"));
-        mSongs.add(new Song("Wavy Glass", "Podington Bear", "Meet Podington Bear", "3:16"));
-        mSongs.add(new Song("Wonder Happens", "Podington Bear", "Meet Podington Bear", "3:08"));
-        mSongs.add(new Song("Otter", "Qusic", "Up", "2:32"));
-        mSongs.add(new Song("Being", "Ryan Andersen", "Solitude", "2:24"));
-        mSongs.add(new Song("Love, Love, Love", "Ryan Andersen", "Solitude", "1:31"));
-        mSongs.add(new Song("Solitude", "Ryan Andersen", "Solitude", "3:26"));
-        mSongs.add(new Song("Awake", "Shaolin Dub", "The Urban Chronicle", "3:00"));
-        mSongs.add(new Song("Concrete Worries", "Shaolin Dub", "The Urban Chronicle", "3:27"));
-        mSongs.add(new Song("Islands", "SPCZ", "Holism", "3:59"));
-        mSongs.add(new Song("Quarks", "SPCZ", "Holism", "4:53"));
-        mSongs.add(new Song("Calva Song", "Wankers United", "Polygon Soup", "4:06"));
+        mSongs.add(new Song("Done with Disco", "Art of Escapism", "Wild Card", "3:10", R.drawable.wildcard));
+        mSongs.add(new Song("Eight Cat", "Art of Escapism", "Wild Card", "3:15", R.drawable.wildcard));
+        mSongs.add(new Song("Little by Little", "Art of Escapism", "Wild Card", "1:50", R.drawable.wildcard));
+        mSongs.add(new Song("Out on Our Own", "Art of Escapism", "Wild Card", "3:16", R.drawable.wildcard));
+        mSongs.add(new Song("Rahjan", "Art of Escapism", "Wild Card", "1:55", R.drawable.wildcard));
+        mSongs.add(new Song("Start Over", "Art of Escapism", "Wild Card", "3:12", R.drawable.wildcard));
+        mSongs.add(new Song("Awestruck", "Cellophane Sam", "Sea Change", "3:58", R.drawable.seachange));
+        mSongs.add(new Song("Deluge", "Cellophane Sam", "Sea Change", "3:51", R.drawable.seachange));
+        mSongs.add(new Song("Early Breath", "Cellophane Sam", "Sea Change", "3:03", R.drawable.seachange));
+        mSongs.add(new Song("Mountains", "Cellophane Sam", "Sea Change", "2:19", R.drawable.seachange));
+        mSongs.add(new Song("The Turnaround Road", "Cellophane Sam", "Sea Change", "3:05", R.drawable.seachange));
+        mSongs.add(new Song("While Looking Up", "Cellophane Sam", "Sea Change", "3:21", R.drawable.seachange));
+        mSongs.add(new Song("Lifeless Worlds", "DASK", "Abiogenesis", "3:20", R.drawable.abiogenesis));
+        mSongs.add(new Song("Mass Forming", "DASK", "Abiogenesis", "7:12", R.drawable.abiogenesis));
+        mSongs.add(new Song("Junk Vultures", "Exquisite Frosting Penmanship", "Transistor Rodeo", "2:08", R.drawable.transistorrodeo));
+        mSongs.add(new Song("Lightbringer", "Magna Ingress", "Aeon 3: Memoriae", "3:27", R.drawable.aeon3memoriae));
+        mSongs.add(new Song("Nuts and Bolts", "Monkey Warhol", "Hannah Banana", "3:27", R.drawable.hannahbanana));
+        mSongs.add(new Song("Stolen Moments", "Monkey Warhol", "Hannah Banana", "2:59", R.drawable.hannahbanana));
+        mSongs.add(new Song("Times of Your Life", "Monkey Warhol", "Hannah Banana", "3:32", R.drawable.hannahbanana));
+        mSongs.add(new Song("Wavy Glass", "Podington Bear", "Meet Podington Bear", "3:16", R.drawable.meetpodingtonbear));
+        mSongs.add(new Song("Wonder Happens", "Podington Bear", "Meet Podington Bear", "3:08", R.drawable.meetpodingtonbear));
+        mSongs.add(new Song("Otter", "Qusic", "Up", "2:32", R.drawable.up));
+        mSongs.add(new Song("Being", "Ryan Andersen", "Solitude", "2:24", R.drawable.solitude));
+        mSongs.add(new Song("Love, Love, Love", "Ryan Andersen", "Solitude", "1:31", R.drawable.solitude));
+        mSongs.add(new Song("Solitude", "Ryan Andersen", "Solitude", "3:26", R.drawable.solitude));
+        mSongs.add(new Song("Awake", "Shaolin Dub", "The Urban Chronicle", "3:00", R.drawable.theurbanchronicle));
+        mSongs.add(new Song("Concrete Worries", "Shaolin Dub", "The Urban Chronicle", "3:27", R.drawable.theurbanchronicle));
+        mSongs.add(new Song("Islands", "SPCZ", "Holism", "3:59", R.drawable.holism));
+        mSongs.add(new Song("Quarks", "SPCZ", "Holism", "4:53", R.drawable.holism));
+        mSongs.add(new Song("Calva Song", "Wankers United", "Polygon Soup", "4:06", R.drawable.polygonsoup));
     }
 
     private void extractAlbums() {
